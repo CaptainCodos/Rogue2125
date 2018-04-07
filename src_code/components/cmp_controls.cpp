86f69841f259310bcb624b8af0619bfd6531010f
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-04-07 16:36:09 +0100

    Commented out parent position, not needed yet.

--- src_code/components/cmp_controls.cpp
+++ src_code/components/cmp_controls.cpp
@@ -6,7 +6,7 @@ using namespace std;
 using namespace sf;
 
 void ControlsComponent::update(double dt) {
-	const auto pos = _parent->getPosition();
+	//const auto pos = _parent->getPosition();
 	float _speed = speed;
 	Vector2f dir = { 0, 0 };
 
