eba3ae8fad71b3ed67e0809e9afa4c4128f39a10
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-04-14 00:50:36 +0100

    Added constructor.

---
+++ src_code/components/cmp_menu.cpp
@@ -0,0 +1,8 @@
+#include "cmp_menu.h"
+
+using namespace std;
+using namespace sf;
+
+MenuComponent::MenuComponent(Entity* p) : Component(p) {
+
+}
---
+++ src_code/components/cmp_menu.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "ecm.h"
+
+class MenuComponent : public Component {
+public:
+	MenuComponent(Entity* p);
+	~MenuComponent() = default;
+private:
+
+};
