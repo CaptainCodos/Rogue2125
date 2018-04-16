824e0c061cfbe31f64a081519b3bf0d79ad44baa
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-04-16 13:22:07 +0100

    Added resolution buttons.

--- src_code/scenes/scene_settings.cpp
+++ src_code/scenes/scene_settings.cpp
@@ -32,6 +32,11 @@ void SettingsScene::Load()
 			"Window Options");
 		dH->SetPos(Vector2f(Engine::getWindowSize().x / 2 - 80.0f, Engine::getWindowSize().y / 2 - 270.0f));
 
+		auto resHeader = makeEntity();
+		auto rH = txt->addComponent<TextComponent>(
+			"Resolution Options");
+		rH->SetPos(Vector2f(Engine::getWindowSize().x / 2 - 120.0f, Engine::getWindowSize().y / 2 + 105.0f));
+
 		btn_fullscreen.reset();
 		btn_fullscreen = new_button("Fullscreen");
 		btn_fullscreen->setPosition(Vector2f(Engine::getWindowSize().x / 2 - 130.0f, Engine::getWindowSize().y / 2 - 200.0f));
@@ -46,6 +51,21 @@ void SettingsScene::Load()
 		btn_settings = new_button("Back to Menu");
 		btn_settings->setPosition(Vector2f(Engine::getWindowSize().x / 2 + 50.0f, Engine::getWindowSize().y / 2 + 350.0f));
 		btn_settings->get_components<TextComponent>()[0]->SetPos(sf::Vector2f(btn_settings->getPosition().x, btn_settings->getPosition().y - 8.0f));
+
+		btn_res_one.reset();
+		btn_res_one = new_button("Res: 1920x1080");
+		btn_res_one->setPosition(Vector2f(Engine::getWindowSize().x / 2 + 450.0f, Engine::getWindowSize().y / 2 + 175.0f));
+		btn_res_one->get_components<TextComponent>()[0]->SetPos(sf::Vector2f(btn_res_one->getPosition().x, btn_res_one->getPosition().y - 8.0f));
+
+		btn_res_two.reset();
+		btn_res_two = new_button("Res: 1600x900");
+		btn_res_two->setPosition(Vector2f(Engine::getWindowSize().x / 2 + 50.0f, Engine::getWindowSize().y / 2 + 175.0f));
+		btn_res_two->get_components<TextComponent>()[0]->SetPos(sf::Vector2f(btn_res_two->getPosition().x, btn_res_two->getPosition().y - 8.0f));
+
+		btn_res_three.reset();
+		btn_res_three = new_button("Res: 1024x768");
+		btn_res_three->setPosition(Vector2f(Engine::getWindowSize().x / 2 - 350.0f, Engine::getWindowSize().y / 2 + 175.0f));
+		btn_res_three->get_components<TextComponent>()[0]->SetPos(sf::Vector2f(btn_res_three->getPosition().x, btn_res_three->getPosition().y - 8.0f));
 	}
 	setLoaded(true);
 }
