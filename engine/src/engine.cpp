84b53051f1a12a17b4ae7235575b023a1c95474c
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-04-16 12:36:54 +0100

    Created fullscreen support (F1).

--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -19,122 +19,134 @@ static float loadingTime;
 static RenderWindow* _window;
 
 void Loading_update(float dt, const Scene* const scn) {
-  //  cout << "Eng: Loading Screen\n";
-  if (scn->isLoaded()) {
-    cout << "Eng: Exiting Loading Screen\n";
-    loading = false;
-  } else {
-    loadingspinner += 220.0f * dt;
-    loadingTime += dt;
-  }
+	//  cout << "Eng: Loading Screen\n";
+	if (scn->isLoaded()) {
+		cout << "Eng: Exiting Loading Screen\n";
+		loading = false;
+	}
+	else {
+		loadingspinner += 220.0f * dt;
+		loadingTime += dt;
+	}
 }
 void Loading_render() {
-  // cout << "Eng: Loading Screen Render\n";
-  static CircleShape octagon(80, 8);
-  octagon.setOrigin(80, 80);
-  octagon.setRotation(loadingspinner);
-  octagon.setPosition(Vcast<float>(Engine::getWindowSize()) * .5f);
-  octagon.setFillColor(Color(255,255,255,(Uint8)min(255.f,40.f*loadingTime)));
-  static Text t("Loading", *Resources::get<sf::Font>("RobotoMono-Regular.ttf"));
-  t.setFillColor(Color(255,255,255,(Uint8)min(255.f,40.f*loadingTime)));
-  t.setPosition(Vcast<float>(Engine::getWindowSize()) * Vector2f(0.4f,0.3f));
-  Renderer::queue(&t);
-  Renderer::queue(&octagon);
+	// cout << "Eng: Loading Screen Render\n";
+	static CircleShape octagon(80, 8);
+	octagon.setOrigin(80, 80);
+	octagon.setRotation(loadingspinner);
+	octagon.setPosition(Vcast<float>(Engine::getWindowSize()) * .5f);
+	octagon.setFillColor(Color(255, 255, 255, (Uint8)min(255.f, 40.f*loadingTime)));
+	static Text t("Loading", *Resources::get<sf::Font>("RobotoMono-Regular.ttf"));
+	t.setFillColor(Color(255, 255, 255, (Uint8)min(255.f, 40.f*loadingTime)));
+	t.setPosition(Vcast<float>(Engine::getWindowSize()) * Vector2f(0.4f, 0.3f));
+	Renderer::queue(&t);
+	Renderer::queue(&octagon);
 }
 
 float frametimes[256] = {};
 uint8_t ftc = 0;
 
 void Engine::Update() {
-  static sf::Clock clock;
-  float dt = clock.restart().asSeconds();
-  {
-    frametimes[++ftc] = dt;
-    static string avg = _gameName + " FPS:";
-    if (ftc % 60 == 0) {
-      double davg = 0;
-      for (const auto t : frametimes) {
-        davg += t;
-      }
-      davg = 1.0 / (davg / 255.0);
-      _window->setTitle(avg + toStrDecPt(2, davg));
-    }
-  }
+	static sf::Clock clock;
+	float dt = clock.restart().asSeconds();
+	{
+		frametimes[++ftc] = dt;
+		static string avg = _gameName + " FPS:";
+		if (ftc % 60 == 0) {
+			double davg = 0;
+			for (const auto t : frametimes) {
+				davg += t;
+			}
+			davg = 1.0 / (davg / 255.0);
+			_window->setTitle(avg + toStrDecPt(2, davg));
+		}
+	}
 
-  if (loading) {
-    Loading_update(dt, _activeScene);
-  } else if (_activeScene != nullptr) {
-    Physics::update(dt);
-    _activeScene->Update(dt);
-  }
+	if (loading) {
+		Loading_update(dt, _activeScene);
+	}
+	else if (_activeScene != nullptr) {
+		Physics::update(dt);
+		_activeScene->Update(dt);
+	}
 }
 
 void Engine::Render(RenderWindow& window) {
-  if (loading) {
-    Loading_render();
-  } else if (_activeScene != nullptr) {
-    _activeScene->Render();
-  }
+	if (loading) {
+		Loading_render();
+	}
+	else if (_activeScene != nullptr) {
+		_activeScene->Render();
+	}
 
-  Renderer::render();
+	Renderer::render();
 }
 
 void Engine::Start(unsigned int width, unsigned int height,
-                   const std::string& gameName, Scene* scn) {
-  RenderWindow window(VideoMode(width, height), gameName);
-  _gameName = gameName;
-  _window = &window;
-  Renderer::initialise(window);
-  Physics::initialise();
+	const std::string& gameName, Scene* scn) {
+	RenderWindow window(VideoMode(width, height), gameName);
+	_gameName = gameName;
+	_window = &window;
+	Renderer::initialise(window);
+	Physics::initialise();
 
-  ChangeScene(scn);
-  while (window.isOpen()) {
-    Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == Event::Closed) {
-        window.close();
-      }
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Escape)) {
-      window.close();
-    }
+	ChangeScene(scn);
+	while (window.isOpen()) {
+		Event event;
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (event.key.code == sf::Keyboard::Escape) {
+					window.close();
+				}
+				else if (event.key.code == sf::Keyboard::F1) {
+					window.create(sf::VideoMode(width, height, 32), gameName, sf::Style::Fullscreen);
+				}
+			}
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+			window.close();
+		}
 
-    window.clear();
-    Update();
-    Render(window);
-    window.display();
-  }
-  if (_activeScene != nullptr) {
-    _activeScene->UnLoad();
-    _activeScene = nullptr;
-  }
-  window.close();
-  Physics::shutdown();
-  // Render::shutdown();
+		window.clear();
+		Update();
+		Render(window);
+		window.display();
+	}
+	if (_activeScene != nullptr) {
+		_activeScene->UnLoad();
+		_activeScene = nullptr;
+	}
+	window.close();
+	Physics::shutdown();
+	// Render::shutdown();
 }
 
 std::shared_ptr<Entity> Scene::makeEntity() {
-  auto e = make_shared<Entity>(this);
-  ents.list.push_back(e);
-  return std::move(e);
+	auto e = make_shared<Entity>(this);
+	ents.list.push_back(e);
+	return std::move(e);
 }
 
 void Engine::setVsync(bool b) { _window->setVerticalSyncEnabled(b); }
 
 void Engine::ChangeScene(Scene* s) {
-  cout << "Eng: changing scene: " << s << endl;
-  auto old = _activeScene;
-  _activeScene = s;
+	cout << "Eng: changing scene: " << s << endl;
+	auto old = _activeScene;
+	_activeScene = s;
 
-  if (old != nullptr) {
-    old->UnLoad(); // todo: Unload Async
-  }
-  if (!s->isLoaded()) {
-    cout << "Eng: Entering Loading Screen\n";
-    loadingTime =0;
-    _activeScene->Load();
-    loading = true;
-  }
+	if (old != nullptr) {
+		old->UnLoad(); // todo: Unload Async
+	}
+	if (!s->isLoaded()) {
+		cout << "Eng: Entering Loading Screen\n";
+		loadingTime = 0;
+		_activeScene->Load();
+		loading = true;
+	}
 }
 
 sf::Vector2u Engine::getWindowSize() { return _window->getSize(); }
@@ -147,18 +159,18 @@ Scene* Engine::GetActiveScene()
 }
 
 namespace timing {
-// Return time since Epoc
-long long now() {
-  return std::chrono::high_resolution_clock::now()
-      .time_since_epoch()
-      .count();
-}
-// Return time since last() was last called.
-long long last() {
-  auto n = now();
-  static auto then = now();
-  auto dt = n - then;
-  then = n;
-  return dt;
-}
+	// Return time since Epoc
+	long long now() {
+		return std::chrono::high_resolution_clock::now()
+			.time_since_epoch()
+			.count();
+	}
+	// Return time since last() was last called.
+	long long last() {
+		auto n = now();
+		static auto then = now();
+		auto dt = n - then;
+		then = n;
+		return dt;
+	}
 } // namespace timing
