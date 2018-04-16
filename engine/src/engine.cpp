9ea75af0252181c43fc7d062ca91f0f1e79322aa
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-04-16 12:44:13 +0100

    Window is now resizeable.

--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -84,7 +84,7 @@ void Engine::Render(RenderWindow& window) {
 
 void Engine::Start(unsigned int width, unsigned int height,
 	const std::string& gameName, Scene* scn) {
-	RenderWindow window(VideoMode(width, height), gameName);
+	RenderWindow window(VideoMode(width, height), gameName, sf::Style::Resize);
 	_gameName = gameName;
 	_window = &window;
 	Renderer::initialise(window);
